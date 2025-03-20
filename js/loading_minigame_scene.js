d730c62e265e6c6879da4ead47abfdd443460c11
MartiCoro4141 <marti.coromina4141@gmail.com>
2025-03-20 12:56:37 +0100

    loading minigame scene
    lololo

---
+++ js/loading_minigame_scene.js
@@ -0,0 +1,42 @@
+let timer;
+let millisInicial;
+let millisFinal;
+let activat;
+
+function setup() {
+  createCanvas(400, 400);
+  textFont('Courier New');
+  textAlign(CENTER, CENTER);
+  activat = false;
+  millisInicial = millis();
+
+ 
+}
+
+function draw() {
+  
+  
+  
+  if(keyIsDown(32)===true){
+      activat = true;
+      millisFinal = 5000+millis();
+      timer = 5000+millis();
+    
+  if(activat){
+    if(millisFinal > 0 ){
+      print(millisFinal);
+      background(220);
+      textSize(24);
+      text('HAS PESCAT UN PEIX!!!', height/2, width/5);
+      textSize(90);
+      text(floor(millisFinal/1000), height/2,width*3/5);
+      millisInicial = millis();
+      millisFinal = timer-millisInicial;
+      }
+    }
+  }
+
+  
+  
+  
+}
